7cb399379c2cf8a3e1c183fb366dd00e940f3474
Phillip Anderson <anderson.pa@gmail.com>
2014-05-05 11:52:49 +0100

    added dropbox info template, fixed ignore list.

---
+++ src/com/pocketleague/manager/db/DbxInfoTemplate.java
@@ -0,0 +1,9 @@
+package com.pocketleague.manager.db;
+
+/** Dropbox application details for cloud integration. */
+public final class DbxInfoTemplate {
+	public static final String appKey = "key";
+	public static final String appSecret = "secret";
+	public static final String appScheme = "db-key";
+
+}
