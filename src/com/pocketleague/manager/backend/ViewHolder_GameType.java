6f4ec296f74e959736fa9a64a2b4404c83e00c3c
Phillip Anderson <anderson.pa@gmail.com>
2014-05-19 23:43:21 +0100

    started converting View_GameTypes to a grid view layout

---
+++ src/com/pocketleague/manager/backend/ViewHolder_GameType.java
@@ -0,0 +1,22 @@
+package com.pocketleague.manager.backend;
+
+public class ViewHolder_GameType {
+	public String name;
+	private int drawable_id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDrawableId() {
+		return drawable_id;
+	}
+
+	public void setDrawableId(int drawable_id) {
+		this.drawable_id = drawable_id;
+	}
+}
