a57918ec79a5b2af940e367c04a4c053308bf486
Phillip Anderson <anderson.pa@gmail.com>
2014-05-19 23:43:21 +0100

    started converting View_GameTypes to a grid view layout

--- src/com/pocketleague/manager/View_GameTypes.java
+++ src/com/pocketleague/manager/View_GameTypes.java
@@ -1,8 +1,6 @@
 package com.pocketleague.manager;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import android.app.Activity;
@@ -17,35 +15,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.GridView;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
-import com.j256.ormlite.dao.Dao;
 import com.pocketleague.gametypes.GameType;
-import com.pocketleague.manager.backend.ListAdapter_Game;
+import com.pocketleague.manager.backend.ListAdapter_GameType;
 import com.pocketleague.manager.backend.NavigationInterface;
-import com.pocketleague.manager.backend.ViewHolderHeader_Game;
-import com.pocketleague.manager.backend.ViewHolder_Game;
+import com.pocketleague.manager.backend.ViewHolder_GameType;
 import com.pocketleague.manager.db.OrmLiteFragment;
-import com.pocketleague.manager.db.tables.Game;
-import com.pocketleague.manager.db.tables.Player;
-import com.pocketleague.manager.db.tables.Session;
-import com.pocketleague.manager.enums.SessionType;
 
 public class View_GameTypes extends OrmLiteFragment {
 	private static final String LOGTAG = "View_GameTypes";
 	NavigationInterface mNav;
 
-	private LinkedHashMap<String, ViewHolderHeader_Game> sHash = new LinkedHashMap<String, ViewHolderHeader_Game>();
-	private List<ViewHolderHeader_Game> sessionList = new ArrayList<ViewHolderHeader_Game>();
-	private ListAdapter_Game gameAdapter;
-	private ExpandableListView elv;
+	private ListAdapter_GameType gameTypeAdapter;
+	private List<ViewHolder_GameType> gametypes_list = new ArrayList<ViewHolder_GameType>();
+	private GridView gv;
 	private Switch viewAllGames;
 	private View rootView;
 	private Context context;
@@ -62,15 +49,15 @@ public class View_GameTypes extends OrmLiteFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		rootView = inflater.inflate(R.layout.activity_view_listing, container,
-				false);
+		rootView = inflater.inflate(R.layout.activity_view_gametypes,
+				container, false);
 
-		elv = (ExpandableListView) rootView.findViewById(R.id.dbListing);
-		gameAdapter = new ListAdapter_Game(context, sessionList);
-		elv.setAdapter(gameAdapter);
-		elv.setOnChildClickListener(elvItemClicked);
-		elv.setOnGroupClickListener(elvGroupClicked);
-		elv.setOnItemLongClickListener(elvItemLongClicked);
+		gv = (GridView) rootView.findViewById(R.id.gametypes_view);
+		gameTypeAdapter = new ListAdapter_GameType(context, R.layout.grid_item,
+				gametypes_list);
+		gv.setAdapter(gameTypeAdapter);
+		// gv.setOnClickListener(elvItemClicked);
+		// gv.setOnItemLongClickListener(elvItemLongClicked);
 
 		viewAllGames = new Switch(context);
 		viewAllGames.setTextOff("Unfinished");
@@ -78,7 +65,7 @@ public class View_GameTypes extends OrmLiteFragment {
 		viewAllGames.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				refreshGamesListing();
+				refreshGameTypesListing();
 			}
 		});
 
@@ -127,183 +114,105 @@ public class View_GameTypes extends OrmLiteFragment {
 	@Override
 	public void onResume() {
 		super.onResume();
-		refreshGamesListing();
+		refreshGameTypesListing();
 	}
 
-	private void expandAll() {
-		// method to expand all groups
-		int count = gameAdapter.getGroupCount();
-		for (int i = 0; i < count; i++) {
-			elv.expandGroup(i);
-		}
-	}
-
-	private void collapseAll() {
-		// method to collapse all groups
-		int count = gameAdapter.getGroupCount();
-		for (int i = 0; i < count; i++) {
-			elv.collapseGroup(i);
-		}
-	}
-
-	protected void refreshGamesListing() {
-		sHash.clear();
-		sessionList.clear();
+	protected void refreshGameTypesListing() {
+		gametypes_list.clear();
 		// add all the sessions to the headers
 
-		Session s;
-		Player[] p = new Player[2];
-
-		try {
-			Dao<Session, Long> sessionDao = Session.getDao(context);
-			Dao<Game, Long> gameDao = Game.getDao(context);
-			Dao<Player, Long> playerDao = Player.getDao(context);
-
-			addSession("Active");
-			addSession("Inactive");
-			for (Session sess : sessionDao) {
-				if (sess.getSessionType() != SessionType.SNGL_ELIM
-						&& sess.getSessionType() != SessionType.DBL_ELIM) {
-					addSession(sess.getSessionName());
-				}
-			}
-
-			// get all games or just incomplete based on switch
-			List<Game> gamesList = new ArrayList<Game>();
-			if (viewAllGames.isChecked()) {
-				gamesList = gameDao.queryForAll();
-			} else {
-				try {
-					gamesList = gameDao.queryBuilder()
-							.orderBy(Game.DATE_PLAYED, true).where()
-							.eq(Game.IS_COMPLETE, false).query();
-				} catch (SQLException e) {
-					Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG)
-							.show();
-				}
-			}
-
-			for (Game g : gamesList) {
-				sessionDao.refresh(g.getSession());
-				s = g.getSession();
-
-				if (s.getSessionType() != SessionType.SNGL_ELIM
-						&& s.getSessionType() != SessionType.DBL_ELIM) {
-					// p[0] = playerDao.queryForId(g.getFirstPlayer().getId());
-					// p[1] = playerDao.queryForId(g.getSecondPlayer().getId());
-
-					addGame(s.getSessionName(), String.valueOf(g.getId()),
-							p[0].getNickName(), p[1].getNickName(),
-							0 + " / " + 0);
-				}
-			}
-
-			for (GameType gt : GameType.values()) {
-				addGame("Active", "1", gt.name(), gt.toString(), "");
-			}
-		} catch (SQLException e) {
-			loge("Retrieval of games/sessions failed", e);
+		for (GameType gt : GameType.values()) {
+			ViewHolder_GameType gtvh = new ViewHolder_GameType();
+			gtvh.setName(gt.name());
+			gtvh.setDrawableId(gt.toDrawableId());
+			gametypes_list.add(gtvh);
 		}
-		expandAll();
-		gameAdapter.notifyDataSetChanged(); // required if list has changed
+
+		// required if list has changed
+		gameTypeAdapter.notifyDataSetChanged();
 	}
+	// private OnChildClickListener elvItemClicked = new OnChildClickListener()
+	// {
+	// public boolean onChildClick(ExpandableListView parent, View v,
+	// int groupPosition, int childPosition, long id) {
+	//
+	// // get the group header
+	// ViewHolderHeader_Game sessionInfo = sessionList.get(groupPosition);
+	// // get the child info
+	// ViewHolder_Game gameInfo = sessionInfo.getGameList().get(
+	// childPosition);
+	// // display it or do something with it
+	// Toast.makeText(
+	// context,
+	// "Selected " + sessionInfo.getName() + "/"
+	// + String.valueOf(gameInfo.getId()),
+	// Toast.LENGTH_SHORT).show();
+	//
+	// prefs_editor
+	// .putString("currentGameType", GameType.BILLIARDS.name());
+	// prefs_editor.commit();
+	//
+	// Toast.makeText(context,
+	// "Game type is now " + GameType.BILLIARDS.name(),
+	// Toast.LENGTH_SHORT).show();
+	//
+	// // load the game in progress screen
+	// // Long gId = Long.valueOf(gameInfo.getId());
+	// // mNav.loadGame(gId);
+	// return true;
+	// }
+	// };
 
-	private OnChildClickListener elvItemClicked = new OnChildClickListener() {
-		public boolean onChildClick(ExpandableListView parent, View v,
-				int groupPosition, int childPosition, long id) {
+	// private OnItemLongClickListener elvItemLongClicked = new
+	// OnItemLongClickListener() {
+	// @Override
+	// public boolean onItemLongClick(AdapterView<?> parent, View view,
+	// int position, long id) {
+	// if (ExpandableListView.getPackedPositionType(id) ==
+	// ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
+	// int groupPosition = ExpandableListView
+	// .getPackedPositionGroup(id);
+	// int childPosition = ExpandableListView
+	// .getPackedPositionChild(id);
+	//
+	// // get the group header
+	// ViewHolderHeader_Game sessionInfo = sessionList
+	// .get(groupPosition);
+	// // get the child info
+	// ViewHolder_Game gameInfo = sessionInfo.getGameList().get(
+	// childPosition);
+	// // display it or do something with it
+	// Toast.makeText(
+	// context,
+	// "Selected " + sessionInfo.getName() + "/"
+	// + String.valueOf(gameInfo.getId()),
+	// Toast.LENGTH_SHORT).show();
+	//
+	// // load the game in progress screen
+	// Long gid = Long.valueOf(gameInfo.getId());
+	// Intent intent = new Intent(context, Detail_Game.class);
+	// intent.putExtra("GID", gid);
+	// startActivity(intent);
+	// return true;
+	// }
+	// return false;
+	// }
+	// };
 
-			// get the group header
-			ViewHolderHeader_Game sessionInfo = sessionList.get(groupPosition);
-			// get the child info
-			ViewHolder_Game gameInfo = sessionInfo.getGameList().get(
-					childPosition);
-			// display it or do something with it
-			Toast.makeText(
-					context,
-					"Selected " + sessionInfo.getName() + "/"
-							+ String.valueOf(gameInfo.getId()),
-					Toast.LENGTH_SHORT).show();
-
-			prefs_editor
-					.putString("currentGameType", GameType.BILLIARDS.name());
-			prefs_editor.commit();
-
-			Toast.makeText(context,
-					"Game type is now " + GameType.BILLIARDS.name(),
-					Toast.LENGTH_SHORT).show();
-
-			// load the game in progress screen
-			// Long gId = Long.valueOf(gameInfo.getId());
-			// mNav.loadGame(gId);
-			return true;
-		}
-	};
-	private OnItemLongClickListener elvItemLongClicked = new OnItemLongClickListener() {
-		@Override
-		public boolean onItemLongClick(AdapterView<?> parent, View view,
-				int position, long id) {
-			if (ExpandableListView.getPackedPositionType(id) == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
-				int groupPosition = ExpandableListView
-						.getPackedPositionGroup(id);
-				int childPosition = ExpandableListView
-						.getPackedPositionChild(id);
-
-				// get the group header
-				ViewHolderHeader_Game sessionInfo = sessionList
-						.get(groupPosition);
-				// get the child info
-				ViewHolder_Game gameInfo = sessionInfo.getGameList().get(
-						childPosition);
-				// display it or do something with it
-				Toast.makeText(
-						context,
-						"Selected " + sessionInfo.getName() + "/"
-								+ String.valueOf(gameInfo.getId()),
-						Toast.LENGTH_SHORT).show();
-
-				// load the game in progress screen
-				Long gid = Long.valueOf(gameInfo.getId());
-				Intent intent = new Intent(context, Detail_Game.class);
-				intent.putExtra("GID", gid);
-				startActivity(intent);
-				return true;
-			}
-			return false;
-		}
-	};
-	private OnGroupClickListener elvGroupClicked = new OnGroupClickListener() {
-		public boolean onGroupClick(ExpandableListView parent, View v,
-				int groupPosition, long id) {
-
-			// ViewHolderHeader_Game sessionInfo =
-			// sessionList.get(groupPosition);
-			// Toast.makeText(context, "Tapped " + sessionInfo.getName(),
-			// Toast.LENGTH_SHORT).show();
-			return false;
-		}
-	};
-
-	private void addSession(String session_name) {
-		ViewHolderHeader_Game vhh_Game = new ViewHolderHeader_Game();
-		vhh_Game.setName(session_name);
-		sessionList.add(vhh_Game);
-		sHash.put(session_name, vhh_Game);
-	}
-
-	private void addGame(String sort, String gameId, String p1, String p2,
-			String score) {
-		logd("addGame() - adding game " + gameId);
-		// find the index of the session header
-		ViewHolderHeader_Game sessionInfo = sHash.get(sort);
-		List<ViewHolder_Game> gameList = sessionInfo.getGameList();
-
-		// create a new child and add that to the group
-		ViewHolder_Game gameInfo = new ViewHolder_Game();
-		gameInfo.setId(gameId);
-		gameInfo.setPlayerOne(p1);
-		gameInfo.setPlayerTwo(p2);
-		gameInfo.setScore(score);
-		gameList.add(gameInfo);
-		sessionInfo.setGameList(gameList);
-	}
+	// private void addGameType(GameType gameId, String p1, String p2, String
+	// score) {
+	// logd("addGame() - adding game " + gameId);
+	// // find the index of the session header
+	// ViewHolderHeader_Game sessionInfo = sHash.get(sort);
+	// List<ViewHolder_Game> gameList = sessionInfo.getGameList();
+	//
+	// // create a new child and add that to the group
+	// ViewHolder_Game gameInfo = new ViewHolder_Game();
+	// gameInfo.setId(gameId);
+	// gameInfo.setPlayerOne(p1);
+	// gameInfo.setPlayerTwo(p2);
+	// gameInfo.setScore(score);
+	// gameList.add(gameInfo);
+	// sessionInfo.setGameList(gameList);
+	// }
 }
---
+++ src/com/pocketleague/manager/backend/ListAdapter_GameType.java
@@ -0,0 +1,72 @@
+package com.pocketleague.manager.backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pocketleague.gametypes.GameType;
+import com.pocketleague.manager.R;
+
+public class ListAdapter_GameType extends ArrayAdapter<ViewHolder_GameType> {
+	private Context context;
+	private int layoutResourceId;
+	private List<ViewHolder_GameType> gametype_list = new ArrayList<ViewHolder_GameType>();
+
+	public ListAdapter_GameType(Context context, int layoutResourceId,
+			List<ViewHolder_GameType> data) {
+		super(context, layoutResourceId, data);
+		this.context = context;
+		this.layoutResourceId = layoutResourceId;
+		this.gametype_list = data;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		GameType gametype_info;
+
+		View gridView;
+		TextView textView;
+		ImageView imageView;
+
+		if (convertView == null) {
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			gridView = new View(context);
+			gridView = inflater.inflate(R.layout.grid_item, null);
+		} else {
+			gridView = convertView;
+		}
+
+		textView = (TextView) gridView.findViewById(R.id.grid_text);
+		imageView = (ImageView) gridView.findViewById(R.id.grid_image);
+
+		textView.setText(gametype_list.get(position).getName());
+		imageView.setImageResource(gametype_list.get(position).getDrawableId());
+
+		return gridView;
+	}
+
+	@Override
+	public int getCount() {
+		return gametype_list.size();
+	}
+
+	@Override
+	public ViewHolder_GameType getItem(int position) {
+		// TODO Auto-generated method stub
+		return super.getItem(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return super.getItemId(position);
+	}
+}
