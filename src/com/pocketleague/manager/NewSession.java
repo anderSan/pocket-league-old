5c5aaa76798ece8b2818142e7f7d024d6f7ece41
Phillip Anderson <anderson.pa@gmail.com>
2014-06-07 12:27:50 +0100

    started updating new_session activity.

--- src/com/pocketleague/manager/NewSession.java
+++ src/com/pocketleague/manager/NewSession.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -35,12 +33,15 @@ public class NewSession extends MenuContainerActivity {
 	Long sId;
 	Session s;
 	Dao<Session, Long> sDao;
+	Dao<SessionMember, Long> smDao;
 
-	TextView name;
-	Spinner spinner_sessionType;
-	Spinner spinner_ruleSet;
-	CheckBox isActiveCB;
-	ListView rosterCheckList;
+	Button btn_create;
+	TextView tv_name;
+	Spinner sp_sessionType;
+	Spinner sp_ruleSet;
+	TextView tv_num_selected;
+	ListView lv_roster;
+	CheckBox cb_isFavorite;
 
 	List<Team> teams = new ArrayList<Team>();
 	List<Integer> teamIdxList = new ArrayList<Integer>();
@@ -51,10 +52,17 @@ public class NewSession extends MenuContainerActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_session);
 
-		name = (TextView) findViewById(R.id.editText_sessionName);
-		Button createButton = (Button) findViewById(R.id.button_createSession);
+		sDao = Session.getDao(this);
+		smDao = SessionMember.getDao(this);
 
-		spinner_sessionType = (Spinner) findViewById(R.id.newSession_sessionType);
+		btn_create = (Button) findViewById(R.id.button_createSession);
+		tv_name = (TextView) findViewById(R.id.editText_sessionName);
+		sp_sessionType = (Spinner) findViewById(R.id.newSession_sessionType);
+		sp_ruleSet = (Spinner) findViewById(R.id.newSession_ruleSet);
+		tv_num_selected = (TextView) findViewById(R.id.tv_num_selected);
+		lv_roster = (ListView) findViewById(R.id.newSession_teamSelection);
+		cb_isFavorite = (CheckBox) findViewById(R.id.newSession_isFavorite);
+
 		List<String> sessionTypes = new ArrayList<String>();
 		for (SessionType st : SessionType.values()) {
 			sessionTypes.add(st.toString());
@@ -64,9 +72,8 @@ public class NewSession extends MenuContainerActivity {
 				Arrays.asList(SessionType.values()));
 		stAdapter
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner_sessionType.setAdapter(stAdapter);
+		sp_sessionType.setAdapter(stAdapter);
 
-		spinner_ruleSet = (Spinner) findViewById(R.id.newSession_spinner_ruleSet);
 		List<String> ruleSetDescriptions = new ArrayList<String>();
 		GameType currentGameType = getCurrentGameType();
 		for (GameRule gr : currentGameType.toGameRules()) {
@@ -77,9 +84,7 @@ public class NewSession extends MenuContainerActivity {
 				ruleSetDescriptions, currentGameType.toGameRules());
 		rsAdapter
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner_ruleSet.setAdapter(rsAdapter);
-
-		isActiveCB = (CheckBox) findViewById(R.id.newSession_isActive);
+		sp_ruleSet.setAdapter(rsAdapter);
 
 		try {
 			teams = Team.getDao(this).queryForAll();
@@ -88,150 +93,119 @@ public class NewSession extends MenuContainerActivity {
 				teamNames.add(t.getTeamName());
 			}
 		} catch (SQLException e) {
-			Toast.makeText(getApplicationContext(), e.getMessage(),
-					Toast.LENGTH_LONG).show();
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
 		}
 
-		Intent intent = getIntent();
-		sId = intent.getLongExtra("SID", -1);
-		if (sId != -1) {
-			try {
-				sDao = Session.getDao(this);
-				s = sDao.queryForId(sId);
-				createButton.setText("Modify");
-				name.setText(s.getSessionName());
-				spinner_sessionType.setVisibility(View.GONE);
-				spinner_ruleSet.setVisibility(View.GONE);
-				isActiveCB.setVisibility(View.VISIBLE);
-				isActiveCB.setChecked(s.getIsActive());
-
-				// TODO: if loading a session, show player/team names or hide
-				// box but dont allow session roster to change or bad things
-				// could happen!
-				teamNames.clear();
-			} catch (SQLException e) {
-				Toast.makeText(getApplicationContext(), e.getMessage(),
-						Toast.LENGTH_LONG).show();
-			}
-		}
-
-		rosterCheckList = (ListView) findViewById(R.id.newSession_playerSelection);
-		rosterCheckList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 		updateRosterCheckList();
-		rosterCheckList.setOnItemClickListener(new OnItemClickListener() {
+		lv_roster.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView arg0, View view, int pos,
 					long arg3) {
-
 				if (teamIdxList.contains(pos)) {
 					teamIdxList.remove((Integer) pos);
 				} else {
 					teamIdxList.add(pos);
 				}
-
-				// String strText = "";
-				//
-				// Collections.sort(teamIdxList);
-				// for (int i = 0; i < teamIdxList.size(); i++)
-				// strText += teams.get(teamIdxList.get(i)).getTeamName()
-				// + ",";
+				tv_num_selected.setText(teamIdxList.size() + " selected");
 			}
 		});
+
+		Intent intent = getIntent();
+		sId = intent.getLongExtra("SID", -1);
+		if (sId != -1) {
+			loadSessionValues();
+		}
+	}
+
+	private void loadSessionValues() {
+		try {
+			s = sDao.queryForId(sId);
+			btn_create.setText("Modify");
+			tv_name.setText(s.getSessionName());
+			sp_sessionType.setVisibility(View.GONE);
+			sp_ruleSet.setVisibility(View.GONE);
+			cb_isFavorite.setChecked(s.getIsFavorite());
+
+			// TODO: if loading a session, show player/team names or hide
+			// box but dont allow session roster to change or bad things
+			// could happen!
+			teamNames.clear();
+		} catch (SQLException e) {
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+		}
 	}
 
 	public void updateRosterCheckList() {
-		rosterCheckList.setAdapter(new ArrayAdapter<String>(this,
+		lv_roster.setAdapter(new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_multiple_choice, teamNames));
 	}
 
-	public void createNewSession(View view) {
-		Context context = getApplicationContext();
-		Session session = null;
-		String session_name = null;
-		SessionType session_type;
-		GameRule game_rule;
-		Boolean is_active = true;
-
-		List<SessionMember> sMembers = new ArrayList<SessionMember>();
-
-		// get the session name
-		String st;
-		st = name.getText().toString().trim().toLowerCase(Locale.US);
-		if (!st.isEmpty()) {
-			session_name = st;
-		}
-
-		// get the session type
-		session_type = (SessionType) spinner_sessionType.getSelectedView()
-				.getTag();
-
-		// get the game rules
-		game_rule = (GameRule) spinner_ruleSet.getSelectedView().getTag();
-
-		int team_size = 1;
-
-		// make the new session or modify an existing one
-		if (sId != -1) {
-			s.setSessionName(session_name);
-			s.setIsActive(isActiveCB.isChecked());
-
-			try {
-				sDao.update(s);
-				Toast.makeText(context, "Session modified.", Toast.LENGTH_SHORT)
-						.show();
-				finish();
-			} catch (SQLException e) {
-				e.printStackTrace();
-				loge("Could not modify session", e);
-				Toast.makeText(context, "Could not modify session.",
-						Toast.LENGTH_SHORT).show();
-			}
+	public void doneButtonPushed(View view) {
+		String session_name = tv_name.getText().toString().trim();
+		if (session_name.isEmpty()) {
+			Toast.makeText(this, "Session name is required.", Toast.LENGTH_LONG)
+					.show();
 		} else {
-			// create the session
-			session = new Session(session_name, getCurrentGameType(),
-					game_rule, session_type, team_size);
+			SessionType session_type = (SessionType) sp_sessionType
+					.getSelectedView().getTag();
+			GameRule game_rule = (GameRule) sp_ruleSet.getSelectedView()
+					.getTag();
+			Boolean is_favorite = cb_isFavorite.isChecked();
 
-			try {
-				sDao = getHelper().getSessionDao();
-				sDao.create(session);
-				Toast.makeText(context, "Session created!", Toast.LENGTH_SHORT)
-						.show();
-			} catch (SQLException e) {
-				loge("Could not create session", e);
-				Toast.makeText(context, "Could not create session.",
-						Toast.LENGTH_SHORT).show();
-			}
-
-			// convert the indices from the roster list to actual teams
-
-			List<Team> roster = new ArrayList<Team>();
-			for (Integer teamIdx : teamIdxList) {
-				roster.add(teams.get(teamIdx));
-			}
-
-			roster = seedRoster(roster);
-
-			int ii = 0;
-			for (Team t : roster) {
-				sMembers.add(new SessionMember(session, t, ii));
-				ii++;
-			}
-
-			// create the session members
-			try {
-				Dao<SessionMember, Long> smDao = getHelper()
-						.getSessionMemberDao();
-				for (SessionMember sm : sMembers) {
-					smDao.create(sm);
-				}
-				finish();
-			} catch (SQLException e) {
-				loge("Could not create session member", e);
-				Toast.makeText(context, "Could not create session member.",
-						Toast.LENGTH_SHORT).show();
+			if (sId != -1) {
+				modifySession(session_name, is_favorite);
+			} else {
+				createSession(session_name, game_rule, session_type,
+						is_favorite);
 			}
 		}
+	}
 
+	private void createSession(String session_name, GameRule game_rule,
+			SessionType session_type, boolean is_favorite) {
+		int team_size = teamIdxList.size();
+		Session newSession = new Session(session_name, getCurrentGameType(),
+				game_rule, session_type, team_size);
+		newSession.setIsFavorite(is_favorite);
+
+		List<Team> roster = new ArrayList<Team>();
+		for (Integer teamIdx : teamIdxList) {
+			roster.add(teams.get(teamIdx));
+		}
+		roster = seedRoster(roster);
+
+		try {
+			sDao.create(newSession);
+			int seed = 0;
+			for (Team t : roster) {
+				SessionMember sm = new SessionMember(newSession, t, seed);
+				smDao.create(sm);
+				seed++;
+			}
+			Toast.makeText(this, "Session created!", Toast.LENGTH_SHORT).show();
+			finish();
+		} catch (SQLException e) {
+			loge("Could not create session", e);
+			Toast.makeText(this, "Could not create session.",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private void modifySession(String session_name, boolean is_favorite) {
+		s.setSessionName(session_name);
+		s.setIsFavorite(is_favorite);
+
+		try {
+			sDao.update(s);
+			Toast.makeText(this, "Session modified.", Toast.LENGTH_SHORT)
+					.show();
+			finish();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			loge("Could not modify session", e);
+			Toast.makeText(this, "Could not modify session.",
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	public List<Team> seedRoster(List<Team> roster) {
